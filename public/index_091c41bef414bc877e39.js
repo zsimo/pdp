3ee5c1b184cf615befcd725f7866c8dc2e66ac7d
Simone <simone.sacchi@nubilaria.com>
2021-01-22 21:29:00 +0100

    add base index style

--- public/index_091c41bef414bc877e39.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";var e,n;e=document.querySelector("main"),n=document.createElement("ul"),["cell","analaogic"].forEach((function(e){var a=document.createElement("li"),t=document.createElement("a");t.href=window.location.href+e,t.innerText="page #"+e,a.appendChild(t),n.appendChild(a)})),e.appendChild(n)})();
