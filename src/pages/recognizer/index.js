214db42e8de8616a2c898996311d52b7cbe45662
Simone <simone.sacchi@nubilaria.com>
2022-07-19 10:57:44 +0200

    working on recognizer page

--- src/pages/recognizer/index.js
+++ src/pages/recognizer/index.js
@@ -2,7 +2,9 @@
 
 const SIZE = 300;
 const INTERVAL = 42;
+const THRESHOLD = 90;
 var canvas;
+var OBJECT_PROP;
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -57,7 +59,75 @@ function getPixelMatrix (dataArray) {
 }
 
 function processMatrix (matrix) {
+        isolateObject(matrix);
+        const box = getBoundingBox(matrix);
+        const boxProp = getBoxProperty(box);
+        OBJECT_PROP = boxProp.aspectRatio;
+        document.getElementById("output").innerHTML = "Aspect ratio: " + OBJECT_PROP.toFixed(2);
+
         updateCanvas(matrix);
+        drawBox(box);
+
+
+}
+
+function getBoxProperty (box) {
+        const prop = {
+                length: 0,
+                width: 0,
+                aspectRatio: 0
+        };
+
+        const deltaX = box.xMax - box.xMin + 1;
+        const deltaY = box.yMax - box.yMin + 1;
+
+        prop.length = Math.max(deltaX, deltaY);
+        prop.width = Math.min(deltaX, deltaY);
+        prop.aspectRatio = prop.width / prop.length;
+
+        return prop;
+}
+
+function drawBox (box) {
+        const context = canvas.getContext("2d");
+        context.beginPath();
+        const deltaX = box.xMax - box.xMin;
+        const deltaY = box.yMax - box.yMin;
+        context.rect(box.xMin, box.yMin, deltaX, deltaY);
+        context.stroke();
+}
+
+function getBoundingBox (matrix) {
+        const bbox = {
+                xMin: SIZE + 1,
+                xMax: 0,
+                yMin: SIZE + 1,
+                yMax: 0
+        };
+        for (var y = 1; y <= SIZE; y++) {
+                for (var x = 1; x <= SIZE; x++) {
+                        if (matrix[y][x] == 0) {
+                                bbox.yMin = Math.min(y, bbox.yMin);
+                                bbox.yMax = Math.max(y, bbox.yMax);
+                                bbox.xMin = Math.min(x, bbox.xMin);
+                                bbox.xMax = Math.max(x, bbox.xMax);
+                        }
+                }
+        }
+
+        return bbox;
+}
+
+function isolateObject (matrix) {
+        for (var i = 1; i <= SIZE; i++) {
+                for (var j = 1; j <= SIZE; j++) {
+                        if (matrix[i][j] < THRESHOLD) {
+                                matrix[i][j] = 0;
+                        } else {
+                                matrix[i][j] = 255
+                        }
+                }
+        }
 }
 
 function updateCanvas (matrix) {
--- src/pages/recognizer/readme.md
+++ src/pages/recognizer/readme.md
@@ -1,2 +1,2 @@
 [How Machine Learning works](https://www.youtube.com/watch?v=QXB1ytG95gs&list=RDCMUC3XGlNq8O5hZlZBJlSFB4jg&start_radio=1)
-5.37
+8.37
