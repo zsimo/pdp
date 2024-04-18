2fecc9fb4aec91fc67fe277a31e9467abe996bad
Simone Sacchi <simone.sacchi@nubilaria.com>
2024-04-18 10:59:01 +0200

    feat(life): first working implementation

--- src/pages/life/world.js
+++ src/pages/life/world.js
@@ -37,81 +37,88 @@ module.exports = {
 };
 
 
+function _iterate () {
+
+    let start = Date.now();
+    const main = document.querySelector("main");
+    const cells = main.querySelectorAll(".cell");
+
+    // const fragment = new DocumentFragment();
+    const newTable = document.createElement("table");
+    const newTbody = newTable.createTBody();
+    //const newTbody = document.createElement("tbody");
+    // fragment.appendChild(newTable);
+    let tr;
+    for (const cell of cells) {
+        let activeCount = 0;
+        const neighbors = [];
+        const rowAbove = cell.parentElement.previousSibling;
+        const rowBelow = cell.parentElement.nextSibling;
+        if (rowAbove) {
+            // topLeft
+            neighbors.push(rowAbove.cells[cell.cellIndex - 1]);
+            // top
+            neighbors.push(rowAbove.cells[cell.cellIndex]);
+            // topRight
+            neighbors.push(rowAbove.cells[cell.cellIndex + 1]);
+        }
+
+        // left
+        neighbors.push(cell.previousSibling);
+        // right
+        neighbors.push(cell.nextSibling);
+        if (rowBelow) {
+            // belowLeft
+            neighbors.push(rowBelow.cells[cell.cellIndex - 1]);
+            // below
+            neighbors.push(rowBelow.cells[cell.cellIndex]);
+            // belowRight
+            neighbors.push(rowBelow.cells[cell.cellIndex + 1]);
+        }
+
+
+        for (const neighbor of neighbors) {
+            if (neighbor && neighbor.classList.contains(ALIVE)) {
+                activeCount += 1;
+            }
+        }
+        const newCell = cell.cloneNode(true);
+        if (activeCount === 2 || activeCount === 3) {
+            newCell.classList.add("alive");
+        } else {
+
+            newCell.classList.remove("alive");
+        }
+        //console.log(cell.parentElement.rowIndex);
+        // const tr = tbody.insertRow(-1);
+        if (!newTbody.rows[cell.parentElement.rowIndex]) {
+            //tr = document.createElement("tr");
+            tr = newTbody.insertRow(-1);
+            // newTbody.appendChild(tr);
+        }
+        tr.appendChild(newCell);
+
+    }
+
+    // //table.replaceChild(table.tBodies[0], newTbody)
+    // table.tBodies[0] = newTbody;
+
+
+    //main.replaceChild(main.querySelector("table"), newTable);
+
+    main.replaceChildren(newTable);
+
+    console.log(Date.now()-start);
+
+}
+
+
 
 const start = document.querySelector("button");
 start.addEventListener("click", function () {
-    setInterval(function () {
-
-        let start = Date.now();
-        const main = document.querySelector("main");
-        const cells = main.querySelectorAll(".cell");
-        const newTable = document.createElement("table");
-        const newTbody = newTable.createTBody();
-        //const fragment = new DocumentFragment();
-        //const newTbody = document.createElement("tbody");
-        //fragment.appendChild(newTbody);
-        let tr;
-        for (const cell of cells) {
-            let activeCount = 0;
-            const neighbors = [];
-            const rowAbove = cell.parentElement.previousSibling;
-            const rowBelow = cell.parentElement.nextSibling;
-            if (rowAbove) {
-                // topLeft
-                neighbors.push(rowAbove.cells[cell.cellIndex - 1]);
-                // top
-                neighbors.push(rowAbove.cells[cell.cellIndex]);
-                // topRight
-                neighbors.push(rowAbove.cells[cell.cellIndex + 1]);
-            }
-
-            // left
-            neighbors.push(cell.previousSibling);
-            // right
-            neighbors.push(cell.nextSibling);
-            if (rowBelow) {
-                // belowLeft
-                neighbors.push(rowBelow.cells[cell.cellIndex - 1]);
-                // below
-                neighbors.push(rowBelow.cells[cell.cellIndex]);
-                // belowRight
-                neighbors.push(rowBelow.cells[cell.cellIndex + 1]);
-            }
-
-
-            for (const neighbor of neighbors) {
-                if (neighbor && neighbor.classList.contains(ALIVE)) {
-                    activeCount += 1;
-                }
-            }
-            const newCell = cell.cloneNode(true);
-            if (activeCount === 2 || activeCount === 3) {
-                newCell.classList.add("alive");
-            } else {
-
-                newCell.classList.remove("alive");
-            }
-            //console.log(cell.parentElement.rowIndex);
-            // const tr = tbody.insertRow(-1);
-            if (!newTbody.rows[cell.parentElement.rowIndex]) {
-                //tr = document.createElement("tr");
-                tr = newTbody.insertRow(-1);
-                // newTbody.appendChild(tr);
-            }
-            tr.appendChild(newCell);
-
-        }
-
-        // //table.replaceChild(table.tBodies[0], newTbody)
-        // table.tBodies[0] = newTbody;
-
-
-        //main.replaceChild(main.querySelector("table"), newTable);
-
-        main.replaceChildren(newTable);
-
-        console.log(Date.now()-start);
-
-    }, INSTANT);
+    requestAnimationFrame(function () {
+        requestAnimationFrame(_iterate);
+    });
+    //setInterval(_iterate, INSTANT);
 })
 
